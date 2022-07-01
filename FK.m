4e96c83135f8a0e03c82640224a4d40f08b96ed9
Walid Shaker <w.khaled@nu.edu.eg>
2022-07-01 19:07:14 +0300

    Update FK.m

--- FK.m
+++ FK.m
@@ -1,4 +1,4 @@
-function [T, T1, T2, T3, T4, T5, T6, T7, Pos] =  FK(q, link_lengths) 
+function [T, T1, T2, T3, T4, T5, T6, Pos] =  FK(q, link_lengths) 
 %% Extracting the link lengths
 d0 = link_lengths(1);
 d1 = link_lengths(2);
@@ -16,21 +16,19 @@ q5 = q(6);
 q6 = q(7);
 
 %% Transformations
-T1 = Ty(q0)* Tz(d0);
+T1 = Ty(q0);
 
-T2 = T1 * Rz(q1) * Tx(d1);
+T2 = T1 * Tz(d0);
 
-T3 = T2 * Ry(q2) * Tx(d2);
+T3 = T2 * Rz(q1) * Tx(d1);
 
-T4 = T3 * Ry(q3) * Tx(d3) * Tz(d4);
+T4 = T3 * Ry(q2) * Tx(d2);
 
-T5 = T4 * Rx(q4);
+T5 = T4 * Ry(q3) * Tx(d3) * Tz(d4);
 
-T6 = T5 * Ry(q5);
+T6 = T5 * Rx(q4)* Ry(q5) * Rx(q6);
 
-T7 = T6 * Rx(q6);
-
-T = T7 * Tx(d6);
+T = T6 * Tx(d6);
 
 phi_x = atan2(T(3,1),T(3,2));
 phi_z = atan2(T(1,3),-T(2,3));
@@ -38,4 +36,3 @@ phi_y = atan2(sqrt(T(1,3)^2+T(2,3)^2),T(3,3));
 
 Pos = [T(1:3,4);phi_x;phi_y;phi_z];
 end
-
